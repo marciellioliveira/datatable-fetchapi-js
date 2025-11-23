9863b2f405045a7bdf0cea1d464dab6ecf166142
Marcielli Oliveira <marciellileticiaol@gmail.com>
2025-11-23 15:37:57 -0300

    Identação

--- js/customDataTable.js
+++ js/customDataTable.js
@@ -2,32 +2,32 @@ async function getData() {
 
     try {
         const records = await fetch("https://dummyjson.com/users");
-    const data = await records.json();
-    
-    let tab = "";
-    data.users.forEach(function(user) {
-        tab += `<tr>
-            <td>${user.firstName}</td>
-            <td>${user.lastName}</td>
-            <td>${user.age}</td>
-            <td>${user.email}</td>
-            <td>${user.gender}</td>
-        </tr>`
-    })
+        const data = await records.json();
+        
+        let tab = "";
+        data.users.forEach(function(user) {
+            tab += `<tr>
+                <td>${user.firstName}</td>
+                <td>${user.lastName}</td>
+                <td>${user.age}</td>
+                <td>${user.email}</td>
+                <td>${user.gender}</td>
+            </tr>`
+        })
 
-    document.getElementById("tbody").innserHTML = tab;
+        document.getElementById("tbody").innserHTML = tab;
 
-    $("#myDataTableJS").DataTable({
-        "data":data.users,
-        "columns":[
-            {"data": "firstName"},
-            {"data": "lastName"},
-            {"data": "age"},
-            {"data": "email"},
-            {"data": "gender"}
+        $("#myDataTableJS").DataTable({
+            "data":data.users,
+            "columns":[
+                {"data": "firstName"},
+                {"data": "lastName"},
+                {"data": "age"},
+                {"data": "email"},
+                {"data": "gender"}
 
-        ]
-    })
+            ]
+        })
     } catch(e) {
         console.log("Erro: ", e)
     }
